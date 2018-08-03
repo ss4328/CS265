04dccfe2994749918545d07333dd7eaf5bcfe47d
ss4328 <ss4328@cs.drexel.edu>
2018-08-03 01:07:12 -0400

    Lab 5 Submission

--- lab5/babynames.py
+++ lab5/babynames.py
@@ -1,7 +1,20 @@
 import csv
 
-with open("babies-first-names-17-10-17.csv","r", encoding="latin-1") as csvfile:
-	reader = csv.DictReader(csvfile)
+with open("babies-first-names-17-10-17.csv","r", encoding="latin-1") as f: 
+	reader = csv.reader(f, delimiter=',')
+	has_header = csv.Sniffer().has_header(f.read(1024))
+	f.seek(0)
+	maximum=0
+	name=''
+	if has_header:
+		next(reader)
 	for row in reader:
-		print(max(columns[3]))
-#
+#		print(row)
+		try:
+			if(isinstance(int(row[3]), int)):
+				if(int(row[3])>maximum):
+					maximum = int(row[3])
+					name = row[2]
+		except ValueError:
+			print("The most popular name was: "+ name +" given to " +str(maximum)+ " children");
+			exit
